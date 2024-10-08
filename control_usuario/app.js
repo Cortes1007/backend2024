0a459b2e0305651928e9e9596b26180cc09add78
Almir de Jesus Cortes Cruz <almircruz030303@gmail.com>
2024-10-08 12:00:53 -0600

    08/10/2024

--- control_usuario/app.js
+++ control_usuario/app.js
@@ -22,11 +22,23 @@ app.get("/usuarios", (req, res)=>{
 
 app.get("/usuarios/:id", (req, res)=>{
     const {id}=req.params;
- 
+    
+    if (isNaN(id)){
+        res.status(400).send({error:"El id debe ser un numero"});
+        return;
+    };
+
     const usuario =usuarios.find((usuario) => usuario.id==id);
+
+    if(usuario === undefined){
+        res.status(404).send({error:`El usuario con id ${id}no existe`});
+        return;
+    }
 
     res.status(200).send(usuario);
 }
+
+
 
 );
 app.listen(3000, ()=>{
