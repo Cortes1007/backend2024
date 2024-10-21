12e8480c04a2e3d76820620c333b626f432a4b32
Almir de Jesus Cortes Cruz <almircruz030303@gmail.com>
2024-10-21 11:32:38 -0600

    guardar

--- control_usuario/app.js
+++ control_usuario/app.js
@@ -2,45 +2,93 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const usuarios = [
-    {
-        id: 1,
-        nombre: "Almir",
-        apellido: "Cortes",
-        email: "ajcc03@gmail.com",
-    },
-    {
-        id: 2,
-        nombre: "Jose",
-        apellido: "Cruz",
-        email: "Jacc02@gmail.com",
-    },
-   ];
-app.get("/usuarios", (req, res)=>{
-   res.status(200).send(usuarios);
+  { id: 1,
+    nombre: "Almir",
+    apellidos: "Cortés",
+    email: "ajcc@gmail.com",
+  },
+
+  { id: 2,
+    nombre: "Jose",
+    apellidos: "Cruz",
+    email: "jacc2@gmail.com",
+  },
+];
+
+app.get("/usuarios",(req,res)=>{
+    
+  res.status(200).send(usuarios);
 });
 
-app.get("/usuarios/:id", (req, res)=>{
-    const {id}=req.params;
+app.get("/usuarios/:id", (req, res) => {
+  const {id} = req.params;
+  
+  if(isNaN(+id)) {
+  res.status(400).send({error: "El id debe ser número"});
+  return
+  };
+
+  //console.log(typeof +id);
+  //console.log(params);
+
+  const usuario = usuarios.find((usuario) => usuario.id === +id);
+
+  if(usuario === undefined){
+    res.status(400).send({error: "El usuario con id ${id} no existe"});
+    return;
+  };
+  
+  res.status(200).send(usuario);
+
+});
+
+app.post("/usuarios",(req, res) => {
+  const {nombre, apellidos, email} = req.body;
+
     
-    if (isNaN(id)){
-        res.status(400).send({error:"El id debe ser un numero"});
-        return;
-    };
+    if (!nombre || !apellidos || !email) {
+      res.status(404).send({ error: "Todos los campos (nombre, apellidos, email) son obligatorios" });
+      return;
+    }
+ 
+  const emailE = usuarios.find((usuario) => usuario.email === email);
+  if (emailE) {
+    res.status(400).send({ error: "El correo electrónico ya está registrado" });
+    return;
+  }
+  
+  usuarios.push({id: usuarios.length +1, nombre, apellidos, email});
+  
+  
+  res.status(201).send("El usuario se agregó correctamente");
 
-    const usuario =usuarios.find((usuario) => usuario.id==id);
+});
 
-    if(usuario === undefined){
-        res.status(404).send({error:`El usuario con id ${id}no existe`});
-        return;
+app.put("/usuarios/:id", (req,res) => {
+  const {nombre, apellidos, email} = req.body;
+    
+    if (!nombre || !apellidos || !email) {
+      res.status(404).send({ error: "Todos los campos (nombre, apellidos, email) son obligatorios" });
+      return;
     }
 
-    res.status(200).send(usuario);
-}
+    usuarios.forEach((usuario) => {
+      if(usuario.id === req.params.id){
+        usuario.nombre = nombre;
+        usuario.apellidos = apellidos;
+        usuario.email = email;
+      }
+    })
+    res.status(200).send("El usuario se actualizo correctamente");
+});
 
+app.patch("/usuarios/:id", (req,res) => {
+  
+});
 
-
-);
-app.listen(3000, ()=>{
-    console.log("servidor corriendo en https://localhost:3000");
+app.listen(3000, () => {
+    console.log("Servidor cooriendo en http://localhost:3000")
 });
